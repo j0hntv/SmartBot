58ab383b83b363983ca8b409a82ecb462d6f68e2
J0hn <j0hntvdesign@gmail.com>
2019-08-24 13:43:55 +0300

    Create telegrambot.py

---
+++ telegrambot.py
@@ -0,0 +1,44 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from dotenv import load_dotenv
+import os
+import logging
+import dialogflow_api
+
+
+def start(bot, update):
+    logging.info(f'Start of dialog {update.message.chat_id}')
+    update.message.reply_text('Привет, я умная железяка, чем могу помочь?')
+
+def reply_to_help(bot, update):
+    logging.info(f'Request for help - ID {update.message.chat_id}')
+    update.message.reply_text('Я бот, созданный в процессе обучения на dvmn.org')
+
+def reply(bot, update):
+    chat_id = update.message.chat_id
+    message = update.message.text
+
+    logging.info(f'Received message from ID {chat_id} - Message: {message}')
+
+    answer = dialogflow_api.get_answer(GOOGLE_PROJECT_ID, chat_id, message, 'ru')
+    update.message.reply_text(answer)
+
+    logging.info(f'Message sent to ID {chat_id} - Message: {answer}')
+
+    
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
+    load_dotenv()
+
+    TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+    GOOGLE_PROJECT_ID = os.getenv('GOOGLE_PROJECT_ID')
+
+    updater = Updater(TELEGRAM_BOT_TOKEN)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', reply_to_help))
+    dp.add_handler(MessageHandler(Filters.text, reply))
+
+    updater.start_polling()
+    updater.idle()
