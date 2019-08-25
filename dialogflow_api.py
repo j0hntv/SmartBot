835798dff64650d0b2d2c7a1fe982bd8510f2ad3
J0hn <j0hntvdesign@gmail.com>
2019-08-25 16:09:25 +0300

    Add create intent function

--- dialogflow_api.py
+++ dialogflow_api.py
@@ -17,3 +17,22 @@ def get_answer(project_id, session_id, text, language_code):
             )
 
     return response.query_result.fulfillment_text
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+    parent = intents_client.project_agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.types.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.types.Intent.Message.Text(text=message_texts)
+    message = dialogflow.types.Intent.Message(text=text)
+
+    intent = dialogflow.types.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message])
+
+    response = intents_client.create_intent(parent, intent)
