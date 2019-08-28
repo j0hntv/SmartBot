4c7026e82948a366c425db26ce59fa61fecebe9e
J0hn <j0hntvdesign@gmail.com>
2019-08-28 13:33:17 +0300

    Add own logger

--- vkbot.py
+++ vkbot.py
@@ -1,10 +1,10 @@
 import os
-import logging
 from dotenv import load_dotenv
 import dialogflow_api
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import random
+import log
 
 
 def reply(event, vk_api):
@@ -15,15 +15,18 @@ def reply(event, vk_api):
             message=answer,
             random_id=random.randint(1,1000)
         )
-        logging.info(f'Message sent to ID {event.user_id} - Message: {answer}')
+        logger.info(f'Message sent to ID {event.user_id} - Message: {answer}')
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
+
     load_dotenv()
 
     VK_TOKEN = os.getenv('VK_TOKEN')
     GOOGLE_PROJECT_ID = os.getenv('GOOGLE_PROJECT_ID')
+
+    logger = log.create_logger('VK-bot')
+    logger.info('Бот заработал.')
 
     vk_session = vk_api.VkApi(token=VK_TOKEN)
     vk_api = vk_session.get_api()
@@ -31,5 +34,5 @@ if __name__ == "__main__":
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            logging.info(f'Received message from ID {event.user_id} - Message: {event.text}')
+            logger.info(f'Received message from ID {event.user_id} - Message: {event.text}')
             reply(event, vk_api)
